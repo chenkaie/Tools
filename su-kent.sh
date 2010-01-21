7c9ac5a91cfa9b1472c1a8fe1617f57c506b162c
Kent Chen <chenkaie@gmail.com>
2010-01-21 15:38:46 +0800

    bug fix

--- su-kent.sh
+++ su-kent.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cp /home/kent/.bashrc /tmp/.bashrc_kent
+cp -rf /home/kent/Repos/DotFiles/.bashrc /tmp/.bashrc_kent
 sed -i -e 's/$HOME/\/home\/kent/g' /tmp/.bashrc_kent
-sed -i -e 's/PS1=\(.*\)/PS1=[\\u@\\h \\W]\\$/g' /tmp/.bashrc_kent
+#sed -i -e 's/PS1=\(.*\)/PS1=[\\u@\\h \\W]\\$/g' /tmp/.bashrc_kent
 source /tmp/.bashrc_kent
 alias vim='vim -u /home/kent/.vimrc'
