1b38153e08e1c113377f52be2688040519104e28
Kent Chen <chenkaie@gmail.com>
2010-07-09 15:27:36 +0800

    Remove "set backup" option in .vimrc to avoid vim showing error message!

--- su-kent-source-me.sh
+++ su-kent-source-me.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-cp -rf /home/kent/Repos/DotFiles/.bashrc /tmp/.bashrc_kent
+
+# .bashrc
+\cp -rf `readlink -f /home/kent/.bashrc` /tmp/.bashrc_kent
 sed -i -e 's/$HOME/\/home\/kent/g' /tmp/.bashrc_kent
 #sed -i -e 's/PS1=\(.*\)/PS1=[\\u@\\h \\W]\\$/g' /tmp/.bashrc_kent
 source /tmp/.bashrc_kent
-alias vim='vim -u /home/kent/.vimrc'
+
+# .vimrc
+\cp -rf `readlink -f /home/kent/.vimrc` /tmp/.vimrc_kent
+sed -i -e '/backup/d' /tmp/.vimrc_kent
+alias vim='vim -u /tmp/.vimrc_kent'
+
