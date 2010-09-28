cc5448dcaa9bbe776fed01d34867399931cb9e14
Kent Chen <chenkaie@gmail.com>
2010-09-28 13:33:15 +0800

    update tunnel.sh script!

--- tunnel.sh
+++ tunnel.sh
@@ -1,2 +1,15 @@
 #/bin/bash
-autossh -M 0 -R 7322:rd1-2:22 root@chenkaie.no-ip.org -p2222 -o TCPKeepAlive=no -o ServerAliveInterval=15
+case "$1" in
+	*dd-wrt*)
+    autossh -M 0 -R 7322:rd1-2:22 root@chenkaie.no-ip.org -p2222 -o TCPKeepAlive=no -o ServerAliveInterval=60
+        ;;
+	*totoro*)
+    autossh -M 0 -R 7321:rd1-2:22 kent@140.118.155.113 -p3063 -o TCPKeepAlive=no -o ServerAliveInterval=60
+        ;;
+	*funp*)
+    autossh -M 0 -R 7322:rd1-2:22 arnold@tcdevweb.funp.com -p22 -o TCPKeepAlive=no -o ServerAliveInterval=60
+        ;;
+	*)
+	exit 1
+	;;
+esac
