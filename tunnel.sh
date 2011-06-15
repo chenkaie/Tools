03896167ee944e7117cf04dc8596f72b2072f445
Kent Chen <chenkaie@gmail.com>
2011-06-15 13:53:57 +0800

    Add proxy mode

--- tunnel.sh
+++ tunnel.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 case "$1" in
+	*dd-wrt-proxy*)
+    # -D [bind_address:]port
+    #    Specifies a local "dynamic" application-level port forwarding.  This works by allocating a socket to listen to port on the local side, optionally bound
+    #    to the specified bind_address.  Whenever a connection is made to this port, the connection is forwarded over the secure channel, and the application
+    #    protocol is then used to determine where to connect to from the remote machine.  Currently the SOCKS4 and SOCKS5 protocols are supported, and ssh will
+    #    act as a SOCKS server.  Only root can forward privileged ports.  Dynamic port forwardings can also be specified in the configuration file.
+
+    autossh -M 0 -D 172.16.5.31:52525 -R 7322:rd1-2:22 root@chenkaie.no-ip.org -p2222 -o TCPKeepAlive=no -o ServerAliveInterval=60
+        ;;
 	*dd-wrt*)
     autossh -M 0 -R 7322:rd1-2:22 root@chenkaie.no-ip.org -p2222 -o TCPKeepAlive=no -o ServerAliveInterval=60
         ;;
