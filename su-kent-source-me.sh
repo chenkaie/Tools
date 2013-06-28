0be68c3c22e98afc3031045390ee8936a23a8f4b
Kent Chen <chenkaie@gmail.com>
2013-06-28 15:28:53 +0800

    Add vim bundle (vundle) support for this script.

--- su-kent-source-me.sh
+++ su-kent-source-me.sh
@@ -11,5 +11,5 @@ source $TMPDIR/.bashrc_kent
 # .vimrc
 \cp -rf `readlink -f /home/kent/.vimrc` $TMPDIR/.vimrc_kent
 sed -i -e '/backup/d' $TMPDIR/.vimrc_kent
-alias vim='vim --cmd "set rtp+=~kent/.vim,~kent/.vim/vundle/" -u '$TMPDIR'/.vimrc_kent'
+alias vim='vim --cmd "set rtp+=~kent/.vim,~kent/.vim/vundle/,~kent/.vim/bundle/" -u '$TMPDIR'/.vimrc_kent'
 
