fddb3e64b132fde9b97f5c2ec3d2d9cb6594333d
Kent Chen <chenkaie@gmail.com>
2014-05-28 14:04:07 +0800

    Use variable string from environment instead of fixed string.
    Read $ME from environment variable first.

--- su-kent-source-me.sh
+++ su-kent-source-me.sh
@@ -2,14 +2,17 @@
 
 TMPDIR=/dev/shm
 
+# Read $ME from environment variable first.
+[ -z "$ME" ] && ME="kent.chen"
+
 # .bashrc
-\cp -rf `readlink -f /home/kent/.bashrc` $TMPDIR/.bashrc_kent
-sed -i -e 's/$HOME/\/home\/kent/g' $TMPDIR/.bashrc_kent
+\cp -rf `readlink -f /home/$ME/.bashrc` $TMPDIR/.bashrc_kent
+sed -i -e 's/$HOME/\/home\/$ME/g' $TMPDIR/.bashrc_kent
 #sed -i -e 's/PS1=\(.*\)/PS1=[\\u@\\h \\W]\\$/g' $TMPDIR/.bashrc_kent
 source $TMPDIR/.bashrc_kent
 
 # .vimrc
-\cp -rf `readlink -f /home/kent/.vimrc` $TMPDIR/.vimrc_kent
+\cp -rf `readlink -f /home/$ME/.vimrc` $TMPDIR/.vimrc_kent
 sed -i -e '/backup/d' $TMPDIR/.vimrc_kent
-alias vim='vim --cmd "set rtp+=~kent/.vim,~kent/.vim/vundle/,~kent/.vim/bundle/" -u '$TMPDIR'/.vimrc_kent'
+alias vim="vim --cmd 'set rtp+=~$ME/.vim,~$ME/.vim/vundle/,~$ME/.vim/bundle/' -u '$TMPDIR'/.vimrc_kent"
 
