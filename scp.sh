d572bb4707876bf2d27f5e4d96f4f57fb0eeab1b
Kent Chen <kent.chen@ubnt.com>
2015-03-01 03:10:49 +0800

    Add handy scp.sh wrapper script to introduce sshpass to config known user:pass first

---
+++ scp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <DEVICE_IP> <FILE_1> <FILE_2>...<FILE_N>"
+	exit 0
+fi
+
+DEVICE_IP="$1"
+
+sshpass -p ${SCP_USER:-ubnt} scp ${@:2} ${SCP_PASS:-ubnt}@$DEVICE_IP:
