cab29d12499ff5f4208ca4cea54ed1ca624b15fe
Kent Chen <kent.chen@ubnt.com>
2015-04-08 17:45:38 +0800

    Support different VID header offset UBI image and do rmmod/insmod each time
    Due to some speical case, kernel moduels have to be rmmod/insmod to make
    it work properly under certern condition, e.g.,
    When you try to ubiattach between two different "VID header offset" image
    GEN3 => UBI: VID header offset: 2048 (aligned 2048), data offset: 4096
    GEN2 => UBI: VID header offset: 512  (aligned 512),  data offset: 2048

--- ubifs-mount-by-sudo.sh
+++ ubifs-mount-by-sudo.sh
@@ -11,25 +11,53 @@ mount_ubi()
 {
 	lsmod |grep nandsim > /dev/null
 	if [ $? -ne 0 ]; then
-		modprobe mtdblock
-		modprobe ubi
-		modprobe nandsim first_id_byte=0x20 second_id_byte=0xaa third_id_byte=0x00 fourth_id_byte=0x15
-		chmod 660 /dev/mtd*
-		mkdir /mnt/ubi/
+		sudo modprobe mtdblock
+		sudo modprobe ubi
+		sudo modprobe nandsim first_id_byte=0x20 second_id_byte=0xaa third_id_byte=0x00 fourth_id_byte=0x15
+		sudo chmod 660 /dev/mtd*
+		sudo mkdir /mnt/ubi > /dev/null 2>&1
 	fi
 
-	dd if=root.ubi of=/dev/mtdblock0
-	ubiattach /dev/ubi_ctrl -m 0
-	mount -t ubifs ubi0_0 /mnt/ubi
+	sudo dd if=$IMAGEDIR/../fw/root.ubi of=/dev/mtdblock0
+
+	# Why does ubiattach on a freshly formatted device fail with "Invalid argument"?
+	# http://www.linux-mtd.infradead.org/faq/ubi.html#L_vid_offset_mismatch
+	case "$GEN" in
+		*gen2*)
+			sudo ubiattach /dev/ubi_ctrl -m 0
+			;;
+		*gen3*)
+			sudo ubiattach /dev/ubi_ctrl -m 0 -O 2048
+			;;
+	esac
+
+	sudo mount -t ubifs ubi0 /mnt/ubi
+	[ $? -eq 0 ] && echo "Mount successfully on: /mnt/ubi"
 }
 
 umount_ubi()
 {
-	umount /mnt/ubi/
-	ubidetach -m 0
+	sudo umount /mnt/ubi/
+	sudo ubidetach -m 0
+
+	# Due to some speical case, kernel moduels have to be rmmod/insmod to make
+	# it work properly under certern condition, e.g.,
+	# When you try to ubiattach between two different "VID header offset" image
+	# GEN3 => UBI: VID header offset: 2048 (aligned 2048), data offset: 4096
+	# GEN2 => UBI: VID header offset: 512  (aligned 512),  data offset: 2048
+
+	lsmod |grep nandsim > /dev/null
+	if [ $? -eq 0 ]; then
+		sudo rmmod ubifs ubi nandsim nand mtdblock mtd_blkdevs cmdlinepart mtd
+	fi
 }
 
 [ $# -le 1 ] || { usage; exit 1; }
+
+if [ -z "${PRODUCTDIR}" ]; then
+	echo "Please source environment file first!"
+	exit 1
+fi
 
 case "$1" in
 	"mount")    mount_ubi  ;;
