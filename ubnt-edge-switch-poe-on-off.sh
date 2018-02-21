c1cb547c5bc1778f81dcb9fab1323e95ea781060
Kent Chen <kent.chen@ubnt.com>
2018-02-21 23:42:13 +0800

    add: ubnt EdgeSwitch and UniFi Switch PoE control helper script

---
+++ ubnt-edge-switch-poe-on-off.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#
+# Usage:
+#   $ ubnt-edge-switch-poe-on-off.sh 192.168.1.20 1
+
+USR=ubnt
+PWD=ubnt
+
+IP="$1"
+LANPORT="$2"
+
+echo "===== Disable PoE port: ${LANPORT} at ${IP} ====="
+# Method 1
+#(echo "${USR}"; echo "${PWD}"; echo "enable"; echo "${PWD}"; echo "configure" ; echo "interface 0/${LANPORT}" ; echo "poe opmode shutdown") | nc ${IP} 23
+
+# Method 2
+#echo -e ${USR}"\n"${PWD}"\n"en"\n"${PWD}"\n"config"\n"interface 0/${LANPORT}"\n"poe opmode shutdown | nc ${IP} 23
+#echo -e "${USR}""\n""${PWD}""\n""enable""\n""${PWD}""\n""config""\n""interface 0/${LANPORT}""\n""poe opmode shutdown" | nc ${IP} 23 > /dev/null 2>&1
+
+# Method 3
+nc ${IP} 23 > /dev/null 2>&1 << EOF
+${USR}
+${PWD}
+enable
+${PWD}
+configure
+interface 0/${LANPORT}
+poe opmode shutdown
+EOF
+
+# Sleep for 500ms
+sleep 0.5
+
+echo "===== Enable  PoE port: ${LANPORT} at ${IP} ====="
+# Method 1
+# (echo "${USR}"; echo "${PWD}"; echo "enable"; echo "${PWD}"; echo "configure" ; echo "interface 0/${LANPORT}" ; echo "poe opmode auto") | nc ${IP} 23
+
+# Method 2
+echo -e "${USR}""\n""${PWD}""\n""enable""\n""${PWD}""\n""config""\n""interface 0/${LANPORT}""\n""poe opmode auto" | nc ${IP} 23 > /dev/null 2>&1
+
---
+++ ubnt-unifi-switch-poe-on-off.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Usage:
+#   $ ubnt-unifi-switch-poe-on-off.sh 192.168.1.20 1
+
+CMD=".cmd.txt"
+USR=ubnt
+PWD=ubnt
+
+IP="$1"
+LANPORT="$2"
+
+echo "===== Disable PoE port: ${LANPORT} at ${IP} ====="
+
+cat > $CMD << EOF
+(echo "enable" ; echo "configure" ; echo "interface 0/$LANPORT" ; echo "poe opmode shutdown"; echo exit; echo exit; echo exit) | telnet localhost 23
+EOF
+
+sshpass -p $PWD ssh -q $USR@$IP $(cat $CMD)
+
+# Sleep for 500ms
+sleep 0.5
+
+echo "===== Enable  PoE port: ${LANPORT} at ${IP} ====="
+
+cat > $CMD << EOF
+(echo "enable" ; echo "configure" ; echo "interface 0/$LANPORT" ; echo "poe opmode auto"; echo exit; echo exit; echo exit) | telnet localhost 23
+EOF
+
+sshpass -p $PWD ssh -q $USR@$IP $(cat $CMD)
+
+rm $CMD
+
