c8a6c5588e7c396126c3dddc0ba694846f0862b8
Kent Chen <kent.chen@ubnt.com>
2019-03-20 15:28:40 +0800

    transfer_gdrive.sh: update and fix 403 Errors issue

--- transfer_gdrive.sh
+++ transfer_gdrive.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-# Ref: https://github.com/prasmussen/gdrive
+# - Ref: https://github.com/prasmussen/gdrive
+# - TUTORIAL: How to get rid of 403 Errors #426
+#   - https://github.com/gdrive-org/gdrive/issues/426
 
-SharedFolder=$(gdrive list --query "name = 'SharedFolderToAnyoneWithLink' and mimeType = 'application/vnd.google-apps.folder'" | sed -n 2p | awk '{print $1}')
+SharedFolder=$(gdrive list --query "name = 'SharedFolderToAnyoneWithLink2019' and mimeType = 'application/vnd.google-apps.folder'" | sed -n 2p | awk '{print $1}')
 if [ "${SharedFolder}" = "" ]; then
-	gdrive mkdir SharedFolderToAnyoneWithLink
-	echo "Just created a 'SharedFolderToAnyoneWithLink' folder on your GDrive, please re-run again"
+	gdrive mkdir SharedFolderToAnyoneWithLink2019
+	echo "Just created a 'SharedFolderToAnyoneWithLink2019' folder on your GDrive, please re-run again"
 else
 	gdrive upload -p "$SharedFolder" --share "$1"
 fi
